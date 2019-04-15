0b5c182ce408c96fc5016fd2f68ff308175cc0ea
zhangqiao <1226937462@qq.com>
2019-04-15 16:35:53 +0800

    initial todo-vue project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Todo />
+</template>
+
+<script>
+import Todo from './components/Todo.vue'
+
+export default {
+  name: 'app',
+  components: {Todo}
+}
+</script>
+
+<style>
+
+</style>
